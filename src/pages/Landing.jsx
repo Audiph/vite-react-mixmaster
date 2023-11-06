7c8dd9a798be5fce9f46025b3d66069d81f5a730
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-07 04:14:21 +0800

    setup getting data from cocktailDB

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,14 @@
+import axios from 'axios';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
 export const loader = async () => {
-  return 'something';
+  const searchTerm = '';
+  const res = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: res.data.drinks, searchTerm };
 };
 
 const Landing = () => {
