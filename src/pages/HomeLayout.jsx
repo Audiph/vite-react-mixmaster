363a21cb65789a5ecfa78f2ae59b96d445fb55ae
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-07 04:29:28 +0800

    modify Navbar import

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import { Navbar } from '../components';
 
 const HomeLayout = () => {
   return (
