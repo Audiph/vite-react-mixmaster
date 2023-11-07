b10bcf5d64955209275e124de1a15c48a4b68988
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-08 02:37:13 +0800

    setup newsletter component

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
 
 const NewsLetter = () => {
-  return <div>NewsLetter</div>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        our newsletter
+      </h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="jeff"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="doe"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="jeff@doe.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default NewsLetter;
