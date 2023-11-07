295e419d60213c3acce75e4bd87efb63b6011a0a
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-08 03:34:52 +0800

    setup useQuery in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,19 +2,36 @@ import axios from 'axios';
 import React, { Fragment } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { CocktailList, SearchForm } from '../components';
+import { useQuery } from '@tanstack/react-query';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const res = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return res.data.drinks;
+    },
+  };
+};
+
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get('search') || '';
-  const res = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: res.data.drinks, searchTerm };
+
+  return { searchTerm };
 };
 
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
+  const { searchTerm } = useLoaderData();
+  const { data: drinks, isLoading } = useQuery(
+    searchCocktailsQuery(searchTerm)
+  );
+
+  if (isLoading) return <h4>Loading...</h4>;
+
   return (
     <Fragment>
       <SearchForm searchTerm={searchTerm} />
