4323fb8408678661516bcbb1a0c394b2854f937b
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-07 22:55:07 +0800

    setup cocktail component loader

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
+
+const singleCocktailUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+
+  return { id, data };
+};
 
 const Cocktail = () => {
+  const { id, data } = useLoaderData();
+
   return <div>Cocktail</div>;
 };
 
