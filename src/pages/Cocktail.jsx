54bae49cf9fb6b8f6cdfdf77faf8d42118a4d259
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-07 23:20:52 +0800

    setup cocktail component

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import { useLoaderData } from 'react-router-dom';
+import { Link, Navigate, useLoaderData } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/CocktailPage';
 
 const singleCocktailUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
@@ -15,7 +16,68 @@ export const loader = async ({ params }) => {
 const Cocktail = () => {
   const { id, data } = useLoaderData();
 
-  return <div>Cocktail</div>;
+  if (!data) return <Navigate to="/" />;
+
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  const validIngredients = Object.keys(singleDrink)
+    .filter(
+      (key) => key.startsWith('strIngredient') && singleDrink[key] !== null
+    )
+    .map((key) => singleDrink[key]);
+
+  console.log(validIngredients);
+
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">ingredients :</span>
+            {validIngredients.length === 1
+              ? validIngredients.join()
+              : validIngredients.join(', ')}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Cocktail;
