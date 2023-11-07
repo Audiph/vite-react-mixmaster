bc6f204edd97e19259ff186947b7cf1ec1b06987
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-08 03:22:09 +0800

    integrate search form

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,8 +6,9 @@ import { CocktailList, SearchForm } from '../components';
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () => {
-  const searchTerm = '';
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get('search') || '';
   const res = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return { drinks: res.data.drinks, searchTerm };
 };
@@ -16,7 +17,7 @@ const Landing = () => {
   const { drinks, searchTerm } = useLoaderData();
   return (
     <Fragment>
-      <SearchForm />
+      <SearchForm searchTerm={searchTerm} />
       <CocktailList drinks={drinks} />
     </Fragment>
   );
